16d55d88ee748f978988723069eca4d3d51c3042
vgatica-eb <vgatica@eventbrite.com>
2021-04-14 14:51:11 -0300

    Matching changes

--- test_server.py
+++ test_server.py
@@ -4,8 +4,6 @@ from server import session
 import server
 from unittest.mock import MagicMock
 from server import add_user
-import server
-# from unittest.mock import MagicMock
 
 
 class TestServer(unittest.IsolatedAsyncioTestCase):
@@ -37,16 +35,18 @@ class TestServer(unittest.IsolatedAsyncioTestCase):
         server.users_connected = {"Usuario Test 1", "Usuario Test 2"}
         server.users_connected.remove("Usuario Test 1")
         self.assertEqual({"Usuario Test 2"}, server.users_connected)
+
+    @parameterized.expand([
         ('/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVXNlciBUZXN0MSJ9.nsn153OEb8vMoieBF91b6h9zORlniwdbJ-RL1exsQs0&action=NULL&msg=NULL',
          'User Test1',)
     ])
-    async def test_add_user_ok(self, path, user):
+    def test_add_user_ok(self, path, user):
         add_user(path)
         self.assertIn(user, server.users_connected)
 
     @parameterized.expand([
         ('/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVXNlciBUZXN0MSJ9.958U6hN_SUIaaGG4YT1f0aiyBODBXZIjGLOevZpY1qs&action=NULL&msg=NULL'),
     ])
-    async def test_add_user_fail(self, path):
+    def test_add_user_fail(self, path):
         with self.assertRaises(Exception):
             add_user(path)
