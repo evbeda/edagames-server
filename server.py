b64c73689bbd37b0fec8dfedcba4d4d453075b6e
vgatica-eb <vgatica@eventbrite.com>
2021-04-14 12:13:03 -0300

    end session

--- server.py
+++ server.py
@@ -1,23 +1,37 @@
-import asyncio
-import websockets
-import jwt
-from urllib.parse import parse_qs
+# import asyncio
+# import websockets
+# import jwt
+# from urllib.parse import parse_qs
 
 
-token_key = 'EDAGame$!2021'
-users_connected = set()
+# token_key = 'EDAGame$!2021'
+# users_connected = set()
 
 
-async def session(websocket, path, token_key):
+# def add_user(path):
+#     path = path[2:]
+#     dict_path = parse_qs(path)
+#     encoded_token = dict_path.get('token')[0].encode()
+#     user_to_connect = jwt.decode(encoded_token, token_key, algorithms=["HS256"])
+#     users_connected.add(user_to_connect.get('user'))
+#     return user_to_connect.get('user')
 
-    path = path[2:]
-    dict_path = parse_qs(path)
-    encoded_token = dict_path.get('token')[0].encode()
-    user_to_connect = jwt.decode(encoded_token, token_key, algorithms=["HS256"])
-    users_connected.add(user_to_connect.get('username'))
+
+# def remove_user(users_to_disconnect):
+#     users_connected.remove(users_to_disconnect)
 
 
-if __name__ == '__main__':
-    start_server = websockets.serve(session, "localhost", 5000)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+# async def session(websocket, path):
+#     client = add_user(path)
+#     while True:
+#         try:
+#             msg = await websocket.recv()
+#             await websocket.send(f'Your msg is {msg}')
+#         except websockets.exceptions.WebSocketException:
+#             remove_user(client)
+
+
+# if __name__ == '__main__':
+#     start_server = websockets.serve(session, "localhost", 5000)
+#     asyncio.get_event_loop().run_until_complete(start_server)
+#     asyncio.get_event_loop().run_forever()
