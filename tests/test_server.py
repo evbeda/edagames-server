dcc10fdeffc490b150d452dba2161ddfd2955978
Olivera Luciano <lolivera@eventbrite.com>
2021-04-20 17:17:34 -0300

    add websocket

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,8 +1,6 @@
 import unittest
-from parameterized import parameterized
 import server.server as server
-from server.server import add_user
-from unittest.mock import MagicMock, patch, AsyncMock
+from unittest.mock import patch
 import os
 import json
 
@@ -24,57 +22,6 @@ class MockTrueFunc(object):
 
 
 class TestServer(unittest.IsolatedAsyncioTestCase):
-
-    @parameterized.expand([
-        (
-            '/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
-            'eyJ1c2VyIjoiVXNlciBUZXN0NCJ9.'
-            'p6MnNJLD5jwTH1C0PvqUb-spfc7XW7xf6gQjSiDrktg&action=NULL&msg=NULL',
-            'User Test4',
-        )
-    ])
-    def test_add_user_token_fail(self, path, user):
-        add_user(path)
-        str_us_con = str(server.users_connected)
-        self.assertNotIn(str_us_con, user)
-
-    @parameterized.expand([
-        (
-            '/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
-            'eyJ1c2VyIjoiVXNlciBUZXN0MSJ9.'
-            'nsn153OEb8vMoieBF91b6h9zORlniwdbJ-RL1exsQs0&action=NULL&msg=NULL',
-            'User Test1',
-        )
-    ])
-    def test_add_user_ok(self, path, user):
-
-        add_user(path)
-        self.assertIn(user, server.users_connected)
-
-    @parameterized.expand([
-        ('/'),
-    ])
-    def test_add_user_path_fail(self, path):
-        with self.assertRaises(Exception):
-            add_user(path)
-
-    def test_remove_user(self):
-        server.users_connected = {"Usuario Test 1", "Usuario Test 2"}
-        server.users_connected.remove("Usuario Test 1")
-        self.assertEqual({"Usuario Test 2"}, server.users_connected)
-
-    @patch("server.server.add_user", return_value="User 1")
-    @patch("server.server.true_func", new_callable=MockTrueFunc)
-    async def test_session_methods(self, mock_true, mock_path):
-        websocket = MagicMock()
-        websocket.accept = AsyncMock()
-        websocket.receive_text = AsyncMock()
-        websocket.send_text = AsyncMock()
-
-        await server.session(websocket)
-        websocket.accept.assert_called()
-        websocket.send_text.assert_called()
-
     @patch('requests.post')
     def test_update_users_in_django(self, post_patched):
         user_list = set(['User 1', 'User 2'])
