0d08d208378f38ea3ca33c7bd5612b51de829cb1
Andres Bonilla <abonilla@eventbrite.com>
2021-04-20 15:59:23 -0300

    Add test for game created notification

--- tests/test_server.py
+++ tests/test_server.py
@@ -88,3 +88,18 @@ class TestServer(unittest.IsolatedAsyncioTestCase):
             server.DJANGO_USERS_URI,
             json=json.dumps(user_dict)
         )
+
+    @patch('requests.post')
+    def test_notify_game_created(self, post_patched):
+        server.notify_game_created(
+            '00000000-0000-0000-0000-000000000001',
+            '123e4567-e89b-12d3-a456-426614174000',
+        )
+
+        post_patched.assert_called_with(
+            server.DJANGO_GAME_URI,
+            json=json.dumps({
+                'challenge_id': '00000000-0000-0000-0000-000000000001',
+                'game_id': '123e4567-e89b-12d3-a456-426614174000',
+            })
+        )
