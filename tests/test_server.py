c236defe3281e2eaa904a92f7bf6b2517fa99a26
Andres Bonilla <abonilla@eventbrite.com>
2021-04-21 16:46:12 -0300

    Extract routes from server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -145,15 +145,3 @@ class TestServer(unittest.IsolatedAsyncioTestCase):
                 'data': data
             })
         )
-
-    @parameterized.expand([
-        ({"challenger": "Ana", "challenged": "Pepe"}, 200),
-    ])
-    async def test_challenge(self, data, status):
-        async with AsyncClient(app=app, base_url="http://test") as ac:
-            response = await ac.post(
-                "/challenge",
-                json=data
-            )
-        assert response.status_code == status
-        assert response.json() == ['Challenge received OK']
