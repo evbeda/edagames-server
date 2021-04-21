b69e4c402c7aa5f91559137c0d87424359b4073f
Olivera Luciano <lolivera@eventbrite.com>
2021-04-21 14:22:37 -0300

    fix test game

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,7 +1,16 @@
 import unittest
 from server.game import Game
+from parameterized import parameterized
 
 
-class TestServer(unittest.TestCase):
-    def setUp(self):
-        self.game = Game('player1', 'player2')
+class TestGame(unittest.TestCase):
+    @parameterized.expand([
+        (
+            'User 1',
+            'User 2',
+        )
+    ])
+    def test_game(self, user, challenged_player):
+        game = Game(user, challenged_player)
+        self.assertEqual(game.player, user)
+        self.assertEqual(game.challenged_player, challenged_player)
