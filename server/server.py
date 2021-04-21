c0135467232922021ce38eb8a69d3a5f7d0ad1f2
Olivera Luciano <lolivera@eventbrite.com>
2021-04-21 16:05:12 -0300

    crete acept_challegen

--- server/server.py
+++ server/server.py
@@ -20,6 +20,14 @@ def notify_game_created(challenge_id, game_id):
     )
 
 
+def acept_challenge(self, data):
+    game_id = data['data']['game_id']
+    for game in games:
+        if game.id == game_id:
+            game.state = 'accepted'
+            notify_game_created(game.challenge_id, game_id)
+
+
 @app.websocket("/ws/")
 async def session(websocket: WebSocket, token):
     client = await manager.connect(websocket, token)
@@ -28,6 +36,8 @@ async def session(websocket: WebSocket, token):
     try:
         while True:
             msg = await websocket.receive_text()
+            if msg['action'] == 'accept_challenge':
+                acept_challenge(msg)
             await manager.broadcast(f'Your msg is {msg}')
     except starlette.websockets.WebSocketDisconnect:
         manager.remove_user(client)
