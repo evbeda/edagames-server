7b5def01ce39a949d19d2a40ec633fdb78d4f2ab
Andres Bonilla <abonilla@eventbrite.com>
2021-04-22 14:22:42 -0300

    Add run script

--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
 import starlette
-import uvicorn
 from fastapi import FastAPI, WebSocket
 import requests
 import json
@@ -33,7 +32,3 @@ async def session(websocket: WebSocket, token):
     except starlette.websockets.WebSocketDisconnect:
         manager.remove_user(client)
         return
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=5000)
