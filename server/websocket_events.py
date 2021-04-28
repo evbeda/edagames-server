743f56c303cf55932c5790bb9b45e8bee2a593f4
Andres Bonilla <abonilla@eventbrite.com>
2021-04-28 15:48:31 -0300

    Add events for listing users

--- server/websocket_events.py
+++ server/websocket_events.py
@@ -1,5 +1,7 @@
 EVENT_SEND_CHALLENGE = 'challenge'
 EVENT_SEND_ERROR = 'error'
 EVENT_SEND_YOUR_TURN = 'your_turn'
+EVENT_LIST_USERS = 'list_users'
 ACCEPT_CHALLENGE = 'accept_challenge'
 MOVEMENTS = 'movements'
+LIST_USERS = 'list_users'
