843d124ef4fa367336fc33e40ecd59daa59cd185
Olivera Luciano <lolivera@eventbrite.com>
2021-04-28 14:01:13 -0300

    refactor Movements

--- server/server_event.py
+++ server/server_event.py
@@ -1,5 +1,8 @@
 from server.game import games, Game
-from server.websockets import notify_error_to_client
+from server.websockets import (
+    notify_error_to_client,
+    notify_your_turn
+)
 from server.web_requests import notify_game_created
 from server.exception import GameIdException
 from server.grpc_adapter import GRPCAdapterFactory
@@ -41,4 +44,22 @@ class Movements(ServerEvent):
         self.nameEvent = 'movements'
 
     async def run(self):
-        pass
+        game_id = self.response.get('data', {}).get('game_id')
+        if game_id is None:
+            return await notify_error_to_client(
+                self.client,
+                str(GameIdException),
+            )
+        for game in games:
+            if game.uuid_game == game_id:
+                data_received = await self.execute_action(game)
+                client = data_received.get('data', {}).get('current_player')
+                data_received.update({'id_game': game.uuid_game})
+                return await notify_your_turn(
+                    client,
+                    data_received
+                )
+
+    async def execute_action(self, game: Game):
+        adapter = await GRPCAdapterFactory.get_adapter(game.name)
+        return await adapter.execute_action(game.uuid_game, self.response)
--- tests/test_server_event.py
+++ tests/test_server_event.py
@@ -5,7 +5,10 @@ from parameterized import parameterized
 from server.game import Game, games
 from server.grpc_adapter import GRPCAdapterFactory
 
-from server.server_event import AcceptChallenge
+from server.server_event import (
+    AcceptChallenge,
+    Movements,
+)
 
 
 class TestServerEvent(unittest.IsolatedAsyncioTestCase):
@@ -27,7 +30,26 @@ class TestServerEvent(unittest.IsolatedAsyncioTestCase):
                         start_patched.assert_called()
 
     async def test_start_game(self):
-        with patch.object(GRPCAdapterFactory, 'get_adapter') as get_adapter_patched:
+        with patch.object(GRPCAdapterFactory, 'get_adapter', new_callable=AsyncMock) as get_adapter_patched:
             await AcceptChallenge({}, 'client').start_game(self.game)
             self.assertEqual(self.game.state, 'accepted')
+            get_adapter_patched.assert_called_with(self.game.name)
+
+    @parameterized.expand([
+        ({"action": "accept_challenge", "data": {"game_id": "c303282d-f2e6-46ca-a04a-35d3d873712d"}},),
+    ])
+    async def test_Movements(self, data):
+        client = 'Test Client 1'
+        with patch('server.server_event.notify_error_to_client', new_callable=AsyncMock):
+            with patch('server.server_event.notify_your_turn', new_callable=AsyncMock):
+                with patch('uuid.uuid4', return_value='c303282d-f2e6-46ca-a04a-35d3d873712d'):
+                    game = Game('player1', 'player2', 123213123)
+                    games.append(game)
+                    with patch.object(Movements, 'execute_action', return_value={}) as start_patched:
+                        await Movements(data, client).run()
+                        start_patched.assert_called()
+
+    async def test_execute_action(self):
+        with patch.object(GRPCAdapterFactory, 'get_adapter', new_callable=AsyncMock) as get_adapter_patched:
+            await Movements({}, 'client').execute_action(self.game)
             get_adapter_patched.assert_called_with(self.game.name)
