df07ea45f19640437067b8568620a6a0a4171a0c
Olivera Luciano <lolivera@eventbrite.com>
2021-04-28 14:01:13 -0300

    refactor Movements

--- server/grpc_adapter.py
+++ server/grpc_adapter.py
@@ -13,6 +13,9 @@ class Client:
     async def create_game(self, players):
         return 'test-00000001'
 
+    async def execute_action(self, id, data_dict):
+        return {'idgame': '123e4567-e89b-12d3-a456-426614174000', 'data': {}}
+
 
 def discover_game(game_name: str):
     return FAKE_SERVICE_DISCOVERY_QUORIDOR_HOST_PORT
