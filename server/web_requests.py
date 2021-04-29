1b40a02c7879464e30b06895144aa96e1b570435
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-29 10:45:18 -0300

    fix notify

--- server/web_requests.py
+++ server/web_requests.py
@@ -4,11 +4,10 @@ import json
 import server.web_urls as web_urls
 
 
-def notify_game_created(challenge_id, game_id):
+def notify_game_created(game_id):
     requests.post(
         web_urls.GAME_URL,
         json=json.dumps({
-            'challenge_id': challenge_id,
             'game_id': game_id,
         })
     )
--- tests/test_web_requests.py
+++ tests/test_web_requests.py
@@ -12,14 +12,12 @@ class TestWebRequests(unittest.TestCase):
     @patch('requests.post')
     def test_notify_game_created(self, post_patched):
         notify_game_created(
-            '00000000-0000-0000-0000-000000000001',
             '123e4567-e89b-12d3-a456-426614174000',
         )
 
         post_patched.assert_called_with(
             web_urls.GAME_URL,
             json=json.dumps({
-                'challenge_id': '00000000-0000-0000-0000-000000000001',
                 'game_id': '123e4567-e89b-12d3-a456-426614174000',
             })
         )
