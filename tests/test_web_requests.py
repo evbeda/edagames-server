7246e9ad34a2b895e9db37d90a24cfa33dfe30ed
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-03 16:30:19 -0300

    web_requests updated

--- tests/test_web_requests.py
+++ tests/test_web_requests.py
@@ -2,7 +2,9 @@ import unittest
 import json
 from unittest.mock import patch
 
-from server.web_requests import notify_game_created
+from server.web_requests import (
+    notify_end_game_to_web,
+)
 
 import server.web_urls as web_urls
 
@@ -10,14 +12,14 @@ import server.web_urls as web_urls
 class TestWebRequests(unittest.TestCase):
 
     @patch('requests.post')
-    def test_notify_game_created(self, post_patched):
-        notify_game_created(
-            '123e4567-e89b-12d3-a456-426614174000',
-        )
-
+    def test_notify_end_game_to_web(self, post_patched):
+        game_id = '123e4567-e89b-12d3-a456-426614174000'
+        data = {'player_1': 3000, 'player_2': 5000}
+        notify_end_game_to_web(game_id, data)
         post_patched.assert_called_with(
             web_urls.GAME_URL,
             json=json.dumps({
-                'game_id': '123e4567-e89b-12d3-a456-426614174000',
+                'game_id': game_id,
+                'data': data,
             })
         )
