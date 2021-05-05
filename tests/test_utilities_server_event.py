26a102624723130c9b212b0dab54211e5924050b
Olivera Luciano <lolivera@eventbrite.com>
2021-05-05 12:46:42 -0300

    refactor test_penalize

--- tests/test_utilities_server_event.py
+++ tests/test_utilities_server_event.py
@@ -4,7 +4,6 @@ from parameterized import parameterized
 
 from server.utilities_server_event import (
     penalize,
-    search_value,
     MovesActions,
 )
 from server.game import Game
@@ -18,16 +17,29 @@ class TestUtilitiesServerEvent(unittest.IsolatedAsyncioTestCase):
         self.game = Game(['player1', 'player2'])
 
     @patch('server.utilities_server_event.GRPCAdapterFactory.get_adapter', new_callable=AsyncMock)
-    async def test_penalize(self, gadapter_patched):
-        with patch('server.utilities_server_event.notify_your_turn', new_callable=AsyncMock) as notify_patched:
-            adapter_patched = AsyncMock()
-            adapter_patched.penalize.return_value = MagicMock()
-            gadapter_patched.return_value = adapter_patched
-            with patch('server.utilities_server_event.asyncio.sleep') as sleep_pached:
-                await penalize(self.game)
-                sleep_pached.assert_called_with(TIME_SLEEP)
-                gadapter_patched.assert_called_with(self.game.name)
-                notify_patched.assert_called_with()
+    @patch('server.utilities_server_event.notify_your_turn', new_callable=AsyncMock)
+    @patch('uuid.uuid4', return_value='c303282d-f2e6-46ca-a04a-35d3d873712d')
+    async def test_penalize(self, uuiid_mock, notify_patched, gadapter_patched):
+        data = MagicMock(
+            game_id='123987',
+            current_player='Juan',
+            turn_data={'turn_token': 'c303282d-f2e6-46ca-a04a-35d3d873712d'},
+        )
+        adapter_patched = AsyncMock()
+        adapter_patched.penalize.return_value = MagicMock(
+            game_id='123987',
+            current_player='Juan',
+            turn_data={},
+        )
+        gadapter_patched.return_value = adapter_patched
+        with patch('server.utilities_server_event.asyncio.sleep') as sleep_pached:
+            await penalize(self.game)
+            sleep_pached.assert_called_with(TIME_SLEEP)
+            gadapter_patched.assert_called_with(self.game.name)
+            notify_patched.assert_awaited_once_with(
+                data.current_player,
+                data.turn_data,
+            )
 
     @parameterized.expand([
         ({"action": "accept_challenge", "data": {"game_id": "c303282d-f2e6-46ca-a04a-35d3d873712d"}},),
@@ -35,7 +47,7 @@ class TestUtilitiesServerEvent(unittest.IsolatedAsyncioTestCase):
     async def test_search_value(self, response):
         client = 'User 1'
         value = 'game_id'
-        value_search = await search_value(response, client, value)
+        value_search = await MovesActions.search_value(response, client, value)
         value_expected = "c303282d-f2e6-46ca-a04a-35d3d873712d"
         self.assertEqual(value_search, value_expected)
 
@@ -46,7 +58,7 @@ class TestUtilitiesServerEvent(unittest.IsolatedAsyncioTestCase):
         with patch('server.utilities_server_event.notify_error_to_client') as notify_patched:
             client = 'User 1'
             value = 'game_id'
-            await search_value(response, client, value)
+            await MovesActions.search_value(response, client, value)
             notify_patched.assert_called_with(client, str(GameIdException))
 
     @patch('server.utilities_server_event.penalize', new_callable=MagicMock, return_value=10)
