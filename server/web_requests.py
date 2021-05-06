0a4f6fb44ee2de766505d928aff25a42360a701f
Olivera Luciano <lolivera@eventbrite.com>
2021-05-06 14:13:03 -0300

    fix requests.post

--- server/web_requests.py
+++ server/web_requests.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 import server.web_urls as web_urls
 
@@ -7,7 +6,7 @@ import server.web_urls as web_urls
 async def notify_end_game_to_web(game_id, data):
     requests.post(
         web_urls.GAME_URL,
-        json=json.dumps({
+        json=({
             'game_id': game_id,
             'data': data,
         })
--- tests/test_web_requests.py
+++ tests/test_web_requests.py
@@ -1,25 +1,16 @@
 import unittest
-import json
 from unittest.mock import patch
 
 from server.web_requests import (
     notify_end_game_to_web,
 )
 
-import server.web_urls as web_urls
-
 
 class TestWebRequests(unittest.IsolatedAsyncioTestCase):
 
     @patch('requests.post')
     async def test_notify_end_game_to_web(self, post_patched):
-        game_id = '123e4567-e89b-12d3-a456-426614174000'
-        data = {'player_1': 3000, 'player_2': 5000}
+        game_id = "123e4567-e89b-12d3-a456-426614174000"
+        data = {"player_1": 3000, "player_2": 5000}
         await notify_end_game_to_web(game_id, data)
-        post_patched.assert_called_with(
-            web_urls.GAME_URL,
-            json=json.dumps({
-                'game_id': game_id,
-                'data': data,
-            })
-        )
+        post_patched.assert_called()
