86a0813cda95f3c3d9b79c0134bcd0bf33f8ef95
Olivera Luciano <lolivera@eventbrite.com>
2021-05-11 10:04:10 -0300

    redis implemented

--- server/environment.py
+++ server/environment.py
@@ -12,6 +12,8 @@ def load_env_var(name: str, default: str = None):
 JWT_TOKEN_KEY = load_env_var('TOKEN_KEY', 'EDAGame$!2021')
 WEB_SERVER_URL = load_env_var('WEB_SERVER_URL', 'localhost')
 WEB_SERVER_PORT = load_env_var('WEB_SERVER_PORT', '8000')
+REDIS_HOST = load_env_var('REDIS_HOST', 'localhost')
+REDIS_LOCAL_PORT = load_env_var('REDIS_LOCAL_PORT', '6379')
 
 # Temporary?
 FAKE_SERVICE_DISCOVERY_QUORIDOR_HOST_PORT = load_env_var('QUORIDOR_HOST_PORT', 'localhost:50051')
