7d8eb8feaa4d43c19a02fe48a67f522d431a2d42
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-11 16:09:42 -0300

    reddis standarized set and mock test

--- server/redis.py
+++ server/redis.py
@@ -2,8 +2,8 @@ import redis
 
 from .environment import REDIS_HOST, REDIS_LOCAL_PORT
 
-redis = redis.Redis(host=REDIS_HOST, port=REDIS_LOCAL_PORT, db=0)
+r = redis.Redis(host=REDIS_HOST, port=REDIS_LOCAL_PORT, db=0)
 
 
-def save_game(game):
-    redis.set(game.game_id, game.to_JSON())
+def save_string(key, value, expire=None):
+    r.set(key, value, ex=expire)
--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,10 +1,15 @@
 import unittest
 from unittest.mock import patch
+import fakeredis
 
 from server.redis import (
-    save_game,
+    save_string,
 )
 
 
 class TestRedis(unittest.TestCase):
-    pass
+    def test_save_string(self):
+        key = 'asd123'
+        value = 1
+        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as mock:
+            save_string(key, value)
