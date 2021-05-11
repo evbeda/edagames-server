42b8fa1cfd91aefea848cc7ab02e970913402ae4
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-11 13:00:30 -0300

    redis save game and test_case

--- server/redis.py
+++ server/redis.py
@@ -3,3 +3,7 @@ import redis
 from .environment import REDIS_HOST, REDIS_LOCAL_PORT
 
 redis = redis.Redis(host=REDIS_HOST, port=REDIS_LOCAL_PORT, db=0)
+
+
+def save_game(game):
+    redis.set(game.game_id, game.to_JSON())
