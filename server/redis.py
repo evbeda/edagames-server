73c71012a3f3edbc2f985f50819480869773132e
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-12 12:53:07 -0300

    AcceptChallenge update

--- server/redis.py
+++ server/redis.py
@@ -3,7 +3,7 @@ from redis.exceptions import DataError
 from uvicorn.config import logger
 
 from .environment import REDIS_HOST, REDIS_LOCAL_PORT
-
+from server.websockets import notify_error_to_client
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_LOCAL_PORT, db=0)
 
@@ -13,3 +13,14 @@ def save_string(key, value, expire=None):
         r.set(key, value, ex=expire)
     except DataError as e:
         logger.error(e)
+
+
+async def get_string(key, client, caller):
+    try:
+        return r.getdel(key)
+    except DataError as e:
+        logger.error(e)
+        await notify_error_to_client(
+            client,
+            f'DataError in {caller}_id, send a str',
+        )
