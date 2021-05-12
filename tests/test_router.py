f6c552cbc0f68fe10472ec4425fa8e6df008bd33
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-11 23:03:37 -0300

    test_challenge update

--- tests/test_router.py
+++ tests/test_router.py
@@ -4,6 +4,7 @@ from parameterized import parameterized
 from httpx import AsyncClient
 
 from server.server import app, manager
+from server.game import Game
 
 
 class TestRouter(unittest.IsolatedAsyncioTestCase):
@@ -11,20 +12,22 @@ class TestRouter(unittest.IsolatedAsyncioTestCase):
         ({"challenger": "Ana", "challenged": "Pepe", "challenge_id": "2138123721"}, 200),
     ])
     async def test_challenge(self, data, status):
-        with patch('uuid.uuid4', return_value='810a84e7'):
+        challenge_id = '810a84e7'
+        to_json = '''{ players: [player_1, player_2]}'''
+        with patch('uuid.uuid4', return_value=challenge_id):
             with patch('server.router.notify_challenge_to_client') as mock_notify_challenge:
-                async with AsyncClient(app=app, base_url="http://test") as ac:
-                    response = await ac.post(
-                        "/challenge",
-                        json=data
-                    )
-                mock_notify_challenge.assert_awaited_once_with(
-                    'Pepe',
-                    'Ana',
-                    '810a84e7',
-                )
-        assert response.status_code == status
-        assert response.json() == data
+                with patch('server.router.save_string') as mock_save:
+                    with patch.object(Game, 'to_JSON', return_value=to_json) as mock_to_JSON:
+                        async with AsyncClient(app=app, base_url="http://test") as ac:
+                            response = await ac.post(
+                                "/challenge",
+                                json=data
+                            )
+        mock_to_JSON.assert_called_once_with()
+        mock_save.assert_called_once_with(challenge_id, to_json)
+        mock_notify_challenge.assert_awaited_once_with('Pepe', 'Ana', challenge_id)
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.json(), data)
 
     async def test_update_users_in_django(self):
         user_list = {"users": ["User 1"]}
