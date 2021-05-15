6a23c1d24a25e7af78cb789d0518a05d0a611096
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-15 20:49:59 -0300

    game and router test updated

--- tests/test_router.py
+++ tests/test_router.py
@@ -5,32 +5,21 @@ from httpx import AsyncClient
 
 from server.server import app, manager
 
-from server.constants import TIME_CHALLENGE
-
 
 class TestRouter(unittest.IsolatedAsyncioTestCase):
     @parameterized.expand([
-        ({"challenger": "Ana", "challenged": "Pepe", "challenge_id": "2138123721"}, 200),
+        ({"challenger": "Ana", "challenged": "Pepe"}, 200),
     ])
     async def test_challenge(self, data, status):
         challenge_id = '810a84e7'
-        to_json = '''{ players: [player_1, player_2]}'''
         with patch('uuid.uuid4', return_value=challenge_id):
-            with patch('server.router.notify_challenge_to_client') as mock_notify_challenge:
-                with patch('server.router.save_string') as mock_save:
-                    with patch('server.router.data_challenge', return_value=to_json) as mock_data:
-                        async with AsyncClient(app=app, base_url="http://test") as ac:
-                            response = await ac.post(
-                                "/challenge",
-                                json=data
-                            )
-        mock_data.assert_called_once_with(['Ana', 'Pepe'])
-        mock_save.assert_called_once_with(
-            'c_' + challenge_id,
-            to_json,
-            TIME_CHALLENGE,
-        )
-        mock_notify_challenge.assert_awaited_once_with('Pepe', 'Ana', challenge_id)
+            with patch('server.router.make_challenge') as mock_make_challenge:
+                async with AsyncClient(app=app, base_url="http://test") as ac:
+                    response = await ac.post(
+                        "/challenge",
+                        json=data
+                    )
+        mock_make_challenge.assert_awaited_once_with(['Ana', 'Pepe'])
         self.assertEqual(response.status_code, status)
         self.assertEqual(response.json(), data)
 
