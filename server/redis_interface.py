5de369b1c6ec91678129527074e3da79f1999cc4
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-18 11:35:58 -0300

    added dict calls

--- server/redis_interface.py
+++ server/redis_interface.py
@@ -15,24 +15,47 @@ from server.constants import (
     PREFIX_LOG,
 )
 
+redis_save_calls = {
+    CHALLENGE_ID: save_string,
+    TURN_TOKEN: save_string,
+    BOARD_ID: save_string,
+    LOG: append_to_stream,
+}
+
+redis_get_calls = {
+    CHALLENGE_ID: get_string,
+    TURN_TOKEN: get_string,
+    BOARD_ID: get_string,
+    # LOG: get_string,
+}
+
+relations = {
+    CHALLENGE_ID: PREFIX_CHALLENGE,
+    TURN_TOKEN: PREFIX_TURN_TOKEN,
+    BOARD_ID: PREFIX_GAME,
+    LOG: PREFIX_LOG,
+}
+
 
 def redis_save(key: str, value, caller: str):
     converted_key = key_conversion(key, caller)
-    if caller == LOG:
-        append_to_stream(converted_key, value)
-    else:
-        save_string(converted_key, value)
+    redis_save_calls[caller](converted_key, value)
 
 
-async def redis_get(key: str, caller: str):
+async def redis_get(key: str, client: str, caller: str):
     converted_key = key_conversion(key, caller)
+    if caller == LOG:
+        #  get log not implemented yet
+        pass
+    else:
+        data = get_string(converted_key)
+        if data is None:
+            pass
+        elif data == -1:
+            pass
+    return data
 
 
 def key_conversion(key: str, caller: str):
-    relations = {
-        CHALLENGE_ID: PREFIX_CHALLENGE,
-        TURN_TOKEN: PREFIX_TURN_TOKEN,
-        BOARD_ID: PREFIX_GAME,
-        LOG: PREFIX_LOG,
-    }
-    return f'{relations.get(caller)}{key}'
+    prefix = relations.get(caller)
+    return f'{prefix}{key}'
