7b2be81a0860f3b285af5434064588fe0ae71f5b
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-05-18 20:12:29 -0300

    logger changes

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -24,7 +24,7 @@ class TestRedis(unittest.TestCase):
     ])
     def test_save_string_valid(self, value, expire, e_value, e_ttl):
         key = 'asd123'
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as mock:
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()) as mock:
             save_string(key, value, expire)
             self.assertEqual(e_value, mock.get(key).decode())
             self.assertEqual(e_ttl, mock.ttl(key))
@@ -37,48 +37,50 @@ class TestRedis(unittest.TestCase):
     ])
     @patch('server.redis.logger.error')
     def test_save_string_exception(self, key, value, mocked):
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()):
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()):
             res = save_string(key, value)
             mocked.assert_called_once()
             self.assertEqual(res, REDIS_ERROR)
 
-    def test_get_string_finded(self):
+    def test_get_string_found(self):
         key = 'test_id'
         value = 'test_value'
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as r_mock:
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()) as r_mock:
             r_mock.set(key, value)
             res = get_string(key)
             self.assertEqual(value, res.decode())
 
-    def test_get_string_finded_parsed(self):
+    def test_get_string_found_parsed(self):
         key = 'test_id'
         value = '{"test_value": 10}'
         expected = {'test_value': 10}
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as r_mock:
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()) as r_mock:
             r_mock.set(key, value)
             res = get_string(key)
             self.assertEqual(expected, res)
 
-    def test_get_string_not_finded(self):
+    @patch('server.redis.logger.info')
+    def test_get_string_not_found(self, mock_logger):
         key = 'test_id_1'
         value = 'test_value'
         wrong_key = 'test_id_2'
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as r_mock:
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()) as r_mock:
             r_mock.set(key, value)
             res = get_string(wrong_key)
             self.assertEqual(res, None)
+            mock_logger.assert_called_once()
 
     @patch('server.redis.logger.error')
     def test_get_string_error(self, mock_logger):
         key = 'test_id'
         value = 'test_value'
         error_key = ['test_id']
-        with patch("server.redis.r", fakeredis.FakeStrictRedis()) as r_mock:
+        with patch("server.redis.redis_data", fakeredis.FakeStrictRedis()) as r_mock:
             r_mock.set(key, value)
             get_string(error_key)
             mock_logger.assert_called_once()
 
-    @patch('server.redis.r')
+    @patch('server.redis.redis_data')
     def test_append_to_stream(self, redis_patched):
         stream = 'some_stream'
         data = {
@@ -95,7 +97,7 @@ class TestRedis(unittest.TestCase):
         redis_patched.xadd.assert_called_with(stream, expected)
 
     @patch('server.redis.logger')
-    @patch('server.redis.r')
+    @patch('server.redis.redis_data')
     def test_append_to_stream_redis_error(self, redis_patched, logger_patched):
         redis_patched.xadd.side_effect = redis.RedisError('test')
         append_to_stream('stream', {'data': 'data'})
