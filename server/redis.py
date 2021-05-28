77157fcea3ec8978292ba9b7e0f1ffe94179e79f
Andres Bonilla <abonilla@eventbrite.com>
2021-05-28 18:11:21 -0300

    Add tests for add to set in redis

--- server/redis.py
+++ server/redis.py
@@ -83,7 +83,7 @@ def get_string(key: str):
         return data
 
 
-def add_to_set(key: str, value: str, _):
+def add_to_set(key: str, value: str, _=None):
     try:
         return redis_data.sadd(key, value)
     except DataError as e:
