212f0745b24849035952de46090a6b48f7b309e6
Andres Bonilla <abonilla@eventbrite.com>
2021-06-09 18:24:18 -0300

    Add queue manager

---
+++ server/queues.py
@@ -0,0 +1,48 @@
+import json
+import pika
+from pika.exchange_type import ExchangeType
+
+from server.connection_manager import manager
+
+from server.environment import RABBIT_HOST, RABBIT_PORT
+from server.constants import RABBIT_CLIENT_EXCHANGE
+
+
+class QueueManager:
+
+    def __init__(self):
+        QueueManager.instance = self
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST, RABBIT_PORT))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(RABBIT_CLIENT_EXCHANGE, ExchangeType.direct)
+        self.channel.queue_declare(auto_delete=True)
+        self.channel.queue_bind('', RABBIT_CLIENT_EXCHANGE)
+
+    @staticmethod
+    def get_instance():
+        return getattr(QueueManager, 'instance', QueueManager())
+
+    async def listen(self):
+        self.channel.basic_consume('', QueueManager.message_callback)
+        self.channel.start_consuming()
+
+    @staticmethod
+    def message_callback(ch, method, properties, body):
+        print(body.decode())
+        event, data = body.decode().split('//')
+        data = json.loads(data)
+        manager.send(method.routing_key, event, data)
+        ch.basic_ack(method.delivery_tag)
+
+    def send(self, client, event, data):
+        self.channel.basic_publish(
+            RABBIT_CLIENT_EXCHANGE,
+            client,
+            '//'.join((event, json.dumps(data)))
+        )
+
+    def register_client(self, client):
+        self.channel.queue_bind('', RABBIT_CLIENT_EXCHANGE, client)
+
+    def unregister_client(self, client):
+        self.channel.queue_unbind('', RABBIT_CLIENT_EXCHANGE, client)
