30836aaee99fce7a4616c849287dfe576014cfc1
Andres Bonilla <abonilla@eventbrite.com>
2022-04-27 16:06:41 -0300

    Define new connection manager skeleton

---
+++ server/apigw_connection_manager.py
@@ -0,0 +1,74 @@
+import asyncio
+import json
+import jwt
+import logging
+
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import JSONResponse
+from uvicorn.config import logger
+
+from server.redis_interface import redis_delete, redis_get, redis_save
+
+import server.constants as websocket_events
+from server.constants import CLIENT_LIST, CLIENT_LIST_KEY
+from .environment import JWT_TOKEN_KEY
+
+from typing import Dict, List
+
+router = APIRouter()
+
+
+class AuthenticationError(BaseException):
+    pass
+
+
+class APIGatewayConnectionManager:
+    instance: 'APIGatewayConnectionManager'
+    client_id_to_bot: Dict
+    bot_to_client_id: Dict
+
+    def __init__(self):
+        self.client_id_to_bot = {}
+        self.bot_to_client_id = {}
+        APIGatewayConnectionManager.instance = self
+
+    def connect(self, client_id: str, token: str):
+        encoded_token = token.encode()
+        try:
+            user_to_connect = jwt.decode(
+                encoded_token,
+                JWT_TOKEN_KEY,
+                algorithms=["HS256"],
+            )
+        except jwt.exceptions.InvalidTokenError:
+            raise AuthenticationError
+
+        bot_name = user_to_connect.get('user')
+        self.client_id_to_bot.update({
+            client_id: bot_name
+        })
+        self.bot_to_client_id.update({
+            bot_name: client_id
+        })
+
+        redis_save(CLIENT_LIST_KEY, bot_name, CLIENT_LIST)
+
+        asyncio.create_task(self.notify_user_list_changed())
+
+    def disconnect(self, client_id: str):
+        pass
+
+    async def notify_user_list_changed(self):
+        pass
+
+    async def broadcast(self, event: str, data: Dict):
+        pass
+
+    async def bulk_send(self, clients: List[str], event: str, data: Dict):
+        pass
+
+    async def send(self, client: str, event: str, data: Dict):
+        pass
+
+    async def _send(self):
+        pass
